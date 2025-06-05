c09c29997ce3528160a5d44692b1f521cfd9fc24
wangchun123 <1281602841@qq.com>
2025-06-05 23:18:42 +0800

    配置

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,19 @@ export default defineConfig({
       //   extensions: ['.ts', '.tsx', '.js', '.jsx'],
       // },
     }),
+    removeConsole(),
   ],
+  build: {
+    sourcemap: true,
+    minify: 'esbuild', // 使用 esbuild 进行压缩，支持更好 tree shaking
+    rollupOptions: {
+      treeshake: true, // 显式开启 tree shaking
+      output: {
+        manualChunks: undefined, // 避免不必要的代码分包，提升 tree shaking 效果
+      },
+    },
+  },
+  server: {
+    sourcemap: true,
+  },
 })
